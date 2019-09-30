29cb9a4745d20de7634afa266372cce436251a64
Alexandre Lages <alexandrelages64@gmail.com>
2019-09-30 16:22:37 -0300

    Arquivo SQLITE

--- apo/settings.py
+++ apo/settings.py
@@ -109,7 +109,7 @@ LOGIN_REDIRECT_URL='/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'front/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
